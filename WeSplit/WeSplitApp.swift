728d3baa54d47151e1fe89a02e94f391735b5e26
MaćKo <maciej.kostrzycki@gmail.com>
2024-03-25 00:46:04 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by MaćKo on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
